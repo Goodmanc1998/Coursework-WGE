b8af97471223c6facc032c399ae09d93e8a62255
Goodmanc1998 <goodmanc1998@gmail.com>
2019-03-28 12:44:24 +0000

    Updates
    Loaded in save 1 & 2, block selection (Needs Updated), Standard Assets,

---
+++ Assets/Scripts/Scene 1/SceneManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+
+    VoxelChunk voxelChunk;
+    public GameObject UI;
+
+    public string fileLoadName;
+
+    bool invetory;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (invetory)
+        {
+            Cursor.visible = true;
+        }
+        else
+            Cursor.visible = false;
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            if(invetory == false)
+            {
+                invetory = true;
+                UI.gameObject.SetActive(true);
+            }
+            else if(invetory)
+            {
+                invetory = false;
+                UI.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void InputText(string newText)
+    {
+        fileLoadName = newText;
+    }
+
+}
